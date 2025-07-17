b147b75b7899974f063113c93f8dee0094a406d5
jschbrt <git@johannes-schubert.de>
2025-07-17 19:12:53 +0200

    reformatted using prettier

--- server/html/src/html.js
+++ server/html/src/html.js
@@ -15,6 +15,37 @@ goog.provide('BlocklyGames.html');
 goog.require('Blockly.Msg');
 goog.require('BlocklyGames');
 
+
+const beeMsg = 
+{
+a:
+"Level 1 bis 4 sind zum Üben.\nHilf der Biene zum Honig!\nNimm die bunten Blöcke und baue den Weg.\nDu hast für diese Levels insgesamt 10 Minuten Zeit.\nDrück auf „Testen“.\nDann siehst du, ob es klappt.\nDrück auf „Abgeben“, wenn es fertig ist.\nViel Spaß! 🐝🍯",
+b:
+"Level 5 und 6 sind ein bisschen schwerer.\nDer Weg ist nicht so einfach.\nHilf der Biene!\nGib ihr die richtigen Blöcke.\nSo findet sie den Weg zum Honig.\nDu hast für diese Levels insgesamt 6 Minuten Zeit.\nDrück auf „Testen“, um zu schauen.\nDrück auf „Abgeben“, wenn es fertig ist.\nDu schaffst das! 🐝🍯",
+c:
+"Level 7 bis 11\nDas Labyrinth bleibt gleich.\nAber der Weg ändert sich!\nBringe die Biene jedes Mal anders zum Honig.\nProbiere in jedem Level einen neuen Weg!\nNimm die bunten Blöcke.\nDu hast für diese Levels insgesamt 10 Minuten Zeit.\nDrück auf „Testen“.\nDann auf „Abgeben“.\nViel Glück! 🐝🍯",
+d:
+"Level 12\nDu hast viele Lösungen gemacht.\nJetzt sag uns:\nWelche Lösung findest du am besten?\nWelche ist auch gut?\nSchreib die Nummer in die Kästchen:\n🟨 Gold = die beste Lösung\n⬜ Silber = die zweitbeste Lösung\nDu hast für dieses Level 2 Minuten Zeit.\nDanke! 😊🐝",
+e:
+"Level 13 – das letzte Level!\nDie Biene will zum Honig.\nAber: Es gibt nur wenige Blöcke!\nKannst du trotzdem den Weg finden?\nDenk gut nach und probier es aus!\nDu hast für dieses Level 4 Minuten Zeit.\nViel Glück! 🐝🍯🌟"
+}
+
+const astroMsg = 
+{
+a:
+"Level 1 bis 4 sind zum Üben.\nHilf der Astronautin zum Raumschiff!\nNimm die bunten Blöcke und baue den Weg.\nDu hast für diese Levels insgesamt 10 Minuten Zeit.\nDrück auf „Testen“.\nDann siehst du, ob es klappt.\nDrück auf „Abgeben“, wenn es fertig ist.\nViel Spaß! 🚀",
+b:
+"Level 5 und 6 sind ein bisschen schwerer.\nDer Weg ist nicht so einfach.\nHilf der Astronautin!\nGib ihr die richtigen Blöcke.\nSo findet sie den Weg zum Raumschiff.\nDu hast für diese Levels insgesamt 6 Minuten Zeit.\nDrück auf „Testen“, um zu schauen.\nDrück auf „Abgeben“, wenn es fertig ist.\nDu schaffst das! 🚀",
+c:
+"Level 7 bis 11\nDas Labyrinth bleibt gleich.\nAber der Weg ändert sich!\nBringe die Astronautin jedes Mal anders zum Raumschiff.\nProbiere in jedem Level einen neuen Weg!\nNimm die bunten Blöcke.\nDu hast für diese Levels insgesamt 10 Minuten Zeit.\nDrück auf „Testen“.\nDann auf „Abgeben“.\nViel Glück! 🚀",
+d:
+"Level 12\nDu hast viele Lösungen gemacht.\nJetzt sag uns:\nWelche Lösung findest du am besten?\nWelche ist auch gut?\nSchreib die Nummer in die Kästchen:\n🟨 Gold = die beste Lösung\n⬜ Silber = die zweitbeste Lösung\nDu hast für dieses Level 2 Minuten Zeit.\nDanke! 😊🚀",
+e:
+"Level 13 – das letzte Level!\nDie Astronautin will zum Raumschiff.\nAber: Es gibt nur wenige Blöcke!\nKannst du trotzdem den Weg finden?\nDenk gut nach und probier es aus!\nDu hast für dieses Level 4 Minuten Zeit.\nViel Glück! 🚀🌟"
+}
+
+//TODO: 
+
 /**
  * Top toolbar for page
  * @param {!Object} ij Injected options.
@@ -158,13 +189,52 @@ BlocklyGames.html.doneDialog = function () {
 
 /**
  * Stop dialog
+ * TODO: add skin and level parameters
  */
 BlocklyGames.html.stopDialog = function () {
+// rewrite to give a message according to the level either from beeMsg or astroMsg depending on the skin
+  let msg = '';
+  let skin = 0; // default skin
+  // get skin from url parameters
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('skin')) {
+    skin = parseInt(urlParams.get('skin'), 10);
+  } else {
+    skin = 0; // default skin
+  } 
+  const level = parseInt(urlParams.get('level'), 10) || 1; // default level is 1
+  if (skin === 1) {
+    if (level === 1) {
+      msg = beeMsg.a;
+    } else if (level === 5) {
+      msg = beeMsg.b;
+    } else if (level === 7) {
+      msg = beeMsg.c;
+    } else if (level === 12) {
+      msg = beeMsg.d;
+    } else if (level === 13) {
+      msg = beeMsg.e;
+    }
+  } else if (skin === 0) {
+    if (level === 1) {
+      msg = astroMsg.a;
+    } else if (level === 5) {
+      msg = astroMsg.b;
+    } else if (level === 7) {
+      msg = astroMsg.c;
+    } else if (level === 12) {
+      msg = astroMsg.d;
+    } else if (level === 13) {
+      msg = astroMsg.e;
+    }
+  } else {
+    msg = 'Unbekanntes Level';
+  }
+
   return `
   <div id="dialogStop" class="dialogHiddenContent">
-    <div id="stopImageDiv">
-    <img id="stopImage" src="common/stop.png">
-      <div id="dialogStopText2">Es geht erst dann weiter, </br> wenn Du dazu aufgefordert wirst.</div>
+    <div id="stopMessageDiv">
+      <div id="dialogStopText2">${msg}</div>
     </div>
     <div id="dialogStopButtons" class="center" style="padding: 1ex 3ex 0">
       <button id="stopOK" class="stop">Weiter</button>
